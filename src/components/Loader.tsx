6a724e471188812088c4296eda254b710d481ce2
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-04-29 22:55:07 +0530

    change loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "./Logo"
 
 const Loader = () => {
   return (
@@ -8,7 +9,7 @@ const Loader = () => {
         <div className="absolute inset-0 rounded-full border-4 border-transparent border-t-blue-600 border-r-purple-600 animate-spin"></div>
         {/* Center dot */}
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-linear-to-r from-blue-600 to-purple-600 rounded-full">
-        <img src="src/assets/logoonly.png" alt="" />
+        <Logo />
         </div>
       </div>
     </div>
